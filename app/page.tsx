4eebc26966f92187357f0991b21457c104923536
Simon Friis Hansen <30860295+flexbugs@users.noreply.github.com>
2025-03-31 10:37:34 +0200

    Set base layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,62 @@
 import { Switch, TextField, FormControlLabel } from "@mui/material";
+import { Box } from "@mui/system";
 import Button from "@mui/material/Button";
 
 export default function Home() {
   return (
-    <>
-      <div id="logo">Logo</div>
-      <div id="controls">
-        <div id="trip-distance">
+    <Box sx={{ display: "flex", flexDirection: "column", gap: 4, margin: 2.5 }}>
+      <Box
+        id="logo"
+        sx={{
+          display: "flex",
+          height: 100,
+          alignItems: "center",
+          justifyContent: "center",
+          bgcolor: "lightgrey",
+        }}
+      >
+        Logo placeholder
+      </Box>
+      <Box
+        id="controls"
+        sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+      >
+        <Box
+          id="trip-distance"
+          sx={{ display: "flex", flexDirection: "column", gap: 1 }}
+        >
           <TextField
             label="Trip distance (km)"
             slotProps={{ htmlInput: { type: "number" } }}
           ></TextField>
           <FormControlLabel control={<Switch />} label="Two-way trip" />
-        </div>
-        <TextField
-          label="Km/liter"
-          slotProps={{ htmlInput: { type: "number" } }}
-        ></TextField>
-        <TextField
-          label="Fuel price (kr/liter)"
-          slotProps={{ htmlInput: { type: "number" } }}
-        ></TextField>
+        </Box>
+        <Box
+          id="fuel"
+          sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+        >
+          <TextField
+            label="Km/liter"
+            slotProps={{ htmlInput: { type: "number" } }}
+          ></TextField>
+          <TextField
+            label="Fuel price (kr/liter)"
+            slotProps={{ htmlInput: { type: "number" } }}
+          ></TextField>
+        </Box>
         <Button variant="contained">Calculate cost</Button>
-      </div>
-      <div id="price">156 DKK</div>
-    </>
+      </Box>
+      <Box
+        id="price"
+        sx={{
+          display: "flex",
+          height: 100,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        [PRICE] DKK
+      </Box>
+    </Box>
   );
 }
