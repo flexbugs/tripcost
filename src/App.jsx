d2661f8670fd5e57e78a59397591adf634801057
Simon Friis Hansen <30860295+flexbugs@users.noreply.github.com>
2025-04-25 12:21:28 +0200

    Add MUI, set dark mode theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Button } from "@mui/material";
 import "./App.css";
 
 function App() {
 	return (
 		<>
 			<h1>TripCost</h1>
+			<Button variant="contained">MUI button</Button>
 		</>
 	);
 }
