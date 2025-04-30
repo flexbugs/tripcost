2ecf7c4ec27994394f0c5926921cec84d1552fe6
Simon Friis Hansen <30860295+simonfriishansen@users.noreply.github.com>
2025-04-30 10:22:08 +0200

    Remove unused local style doc, update MUI button

--- src/components/CalcPriceButton.tsx
+++ src/components/CalcPriceButton.tsx
@@ -4,7 +4,7 @@ import { Box, Button } from "@mui/material";
 export default function CalcPriceButton() {
 	return (
 		<Box id="CalcPriceButton">
-			<Button type="submit" variant="contained" size="large">
+			<Button type="submit" fullWidth={true} variant="contained" size="large">
 				Calculate price
 			</Button>
 		</Box>
